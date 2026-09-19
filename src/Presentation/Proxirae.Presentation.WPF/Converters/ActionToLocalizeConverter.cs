94087e2441669acaecf50ec71ed09a12bc4c9528
fralorange <fralorange@mail.ru>
2026-09-19 03:06:16 +0300

    Fix localization bugs 1. Fix converters not updating after changing culture

--- src/Presentation/Proxirae.Presentation.WPF/Converters/ActionToLocalizeConverter.cs
+++ src/Presentation/Proxirae.Presentation.WPF/Converters/ActionToLocalizeConverter.cs
@@ -1,39 +1,37 @@
 ﻿using Proxirae.Contracts.DTOs.Rules.Actions;
+using RentADeveloper.ResXLocalization;
 using System.Globalization;
 using System.Windows.Data;
-using LocalizationKeys = Proxirae.Presentation.WPF.Resources.Localization.Localization;
 
 namespace Proxirae.Presentation.WPF.Converters
 {
-    public class ActionToLocalizeConverter : IValueConverter
+    public class ActionToLocalizeConverter : IMultiValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is BaseActionDto action)
+            if (values.Length == 0 || values[0] is not BaseActionDto action)
+                return string.Empty;
+
+            if (action.Name == "Direct")
+                return Localizer.Current.Get("ActionNameDirect");
+
+            if (action.Name == "Block")
+                return Localizer.Current.Get("ActionNameBlock");
+
+            if (action.Name.StartsWith("Proxy "))
             {
-                if (action.Name == "Direct")
-                    return LocalizationKeys.ActionNameDirect;
+                string proxyWord = Localizer.Current.Get("ActionNameProxy");
+                string restOfName = action.Name.Substring(6);
 
-                if (action.Name == "Block")
-                    return LocalizationKeys.ActionNameBlock;
-
-                if (action.Name.StartsWith("Proxy "))
-                {
-                    string proxyWord = LocalizationKeys.ActionNameProxy;
-                    string restOfName = action.Name.Substring(6);
-
-                    return $"{proxyWord} {restOfName}";
-                }
-
-                return action.Name; 
+                return $"{proxyWord} {restOfName}";
             }
 
-            return string.Empty;
+            return action.Name;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
     }
 }
